160b98fdf4a4765591b6a3ab3eb2fa2e828f2bd1
Sebastian Sarmiento <sebastiansarmiento@Sebastians-MacBook-Pro.local>
2018-09-20 16:50:56 -0300

    Fixes

--- prueba/app.js
+++ prueba/app.js
@@ -3,18 +3,20 @@ let nota = Array.from(notas)
 
 window.onscroll = showNotes
 
-function showNotes(){
-    window.scrollY > 400 ? showElement(nota[0]) : hideElement(nota[0])
-    window.scrollY > 900 ? showElement(nota[1]) : hideElement(nota[1])
-    window.scrollY > 1400 ? showElement(nota[2]) : hideElement(nota[2])
+function showNotes() {
+    window.scrollY > 500 ? showElement(nota[0]) : hideElement(nota[0])
+    window.scrollY > 1000 ? showElement(nota[1]) : hideElement(nota[1])
+    window.scrollY > 1500 ? showElement(nota[2]) : hideElement(nota[2])
 }
 
-function showElement(el){
+function showElement(el) {
     el.style.opacity = '1'
     el.style.transform = 'translateX(0%)'
 }
 
-function hideElement(el){
-    el.style.opacity = '0'
-    el.style.transform = 'translateX(20%)'
+function hideElement(el) {
+    if (el.style.opacity !== '0' && el.style.transform !== 'translateX(20%)') {
+        el.style.opacity = '0'
+        el.style.transform = 'translateX(20%)'
+    }
 }
