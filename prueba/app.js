a9ef9c8b0c08591cd5ce7e09354afc7a33f82e20
Sebastian Sarmiento <sebastiansarmiento@Sebastians-MacBook-Pro.local>
2018-09-21 16:24:21 -0300

    Desktop version

--- prueba/app.js
+++ prueba/app.js
@@ -1,9 +1,14 @@
-let notas = document.getElementsByClassName('nota')
+let notas = document.getElementsByClassName('nota-mobile')
 let nota = Array.from(notas)
+let mobile;
 
-window.onscroll = showNotes
+window.innerWidth < 864 ? mobile = true : mobile = false
 
-function showNotes() {
+mobile ? window.onscroll = showNotesMobile : window.onscroll = showNotesDesktop
+
+!mobile ? window.onload = presentacion : null
+
+function showNotesMobile() {
     window.scrollY > 500 ? showElement(nota[0]) : hideElement(nota[0])
     window.scrollY > 1000 ? showElement(nota[1]) : hideElement(nota[1])
     window.scrollY > 1500 ? showElement(nota[2]) : hideElement(nota[2])
@@ -14,9 +19,26 @@ function showElement(el) {
     el.style.transform = 'translateX(0%)'
 }
 
-function hideElement(el) {
+function hideElement(el, dir) {
     if (el.style.opacity !== '0' && el.style.transform !== 'translateX(20%)') {
         el.style.opacity = '0'
-        el.style.transform = 'translateX(20%)'
+        dir ? el.style.transform = 'translateX(20%)' : el.style.transform = 'translateX(-20%)'
     }
+}
+
+// DESKTOP
+
+let notasDesktop = document.getElementsByClassName('nota')
+let notaDesktop = Array.from(notasDesktop)
+
+function showNotesDesktop(){
+    window.scrollY > 500 ? showElement(notaDesktop[0]) : hideElement(notaDesktop[0], 0)
+    window.scrollY > 500 ? showElement(notaDesktop[1]) : hideElement(notaDesktop[1], 1)
+    window.scrollY > 900 ? showElement(notaDesktop[2]) : hideElement(notaDesktop[2], 0)
+    window.scrollY > 900 ? showElement(notaDesktop[3]) : hideElement(notaDesktop[3], 1)
+}
+
+function presentacion(){
+    document.getElementById('presentacion-texto').style.opacity = '1'
+    document.getElementById('presentacion-texto').style.transform = 'translateX(0%)'
 }
